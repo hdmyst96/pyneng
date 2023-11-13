eece4ff4e3b2fa9347d643ada0b5d76750f75c17
hdmyst96 <dmitryhortolomei@gmail.com>
2023-11-13 20:43:30 +0100

    task 5 done

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,14 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+command1 = command1.split()
+command2 = command2.split()
+
+command1 = command1[-1].split(',') 
+command2 = command2[-1].split(',')
+
+result = command1 + command2
+result.sort()
+result = result[0::3]
+print(result)
