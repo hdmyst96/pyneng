e9916832dabbb4eb9b07f614ecf32b1e7ae87244
hdmyst96 <dmitryhortolomei@gmail.com>
2023-11-21 01:36:44 +0100

    task 5.2 DONE

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Задание 5.2
@@ -30,14 +31,53 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+#chiedere al utente di inserire il ip ed il mask
 ipmask = input('insert ip and mask: ')
-ipmasklist = ipmask.split('/')
-ip = ipmasklist[0:1]
-ipmasklist[0:1]
 
+#dividere ip dal mask
+ipmask = ipmask.split('/')
+ip = ipmask[0:1]
+ip = '.'.join(ip)
+ip = ip.split('.')
+
+#convertire il ip in nr binario 
 network_template = '''
-    ...: Network
-    ...: {:8b} {:8b} {:8b} {:8b}
-    ...: '''
+     Network:
+     {0:<8} {1:<8} {2:<8} {3:<8}
+     {0:08b} {1:08b} {2:08b} {3:08b}
+     '''
+#mostrare ip in nr binario e normale
+print(network_template.format(int(ip[0]),int(ip[1]),int(ip[2]),int(ip[3])))
+
+#incapsulre il mask in una variabile 
+maskd = ipmask[1:2]
+mask = ipmask[1:2]
+
+mask = ''.join(mask)
+#convertire il mask in numero binario 
+mask = "1" * int(mask) + "0" * 4  
+
+#controllare se il mask ha 32 bit, nel caso aggiungere i 0 mancanti
+mask =  mask.ljust(32, "0")
+
+#aggiungere i punti tra i otteti
+mask = mask[0:8] + "." + mask[8:16] + "." +  mask[16:24] + "." + mask[24
+:32]
+mask =  mask.split('.')
+
+# usare la mask nel template 
+mask_template = '''
+     Mask:
+     {0:}
+     {5:<8} {6:<8} {7:<8} {8:<8}
+     {1:08} {2:08} {3:08} {4:08}
+                '''
+
+
+print(mask_template.format(int(maskd[0]), int(mask[0]), int(mask[1]), int
+(mask[2]), int(mask[3]), int(mask[0],2), int(mask[1],2), int(mask[2],2)
+, int(mask[3],2)))
+
+
 
 
