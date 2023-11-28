e5f8f857954293d63b2dd11b64e83c38c7deccd8
hdmyst96 <dmitryhortolomei@gmail.com>
2023-11-28 18:22:13 +0100

    task6_2b DONE!

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3 
 # -*- coding: utf-8 -*-
 """
 Задание 6.2b
@@ -12,3 +13,35 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Inserisci IP: ')
+ip_list = ip.split('.')
+ip_correct = False
+#Verification programm
+while not ip_correct:
+    oct = ip.split('.')[0]
+    ip_list = ip.split('.')
+    if len(ip_list) != 4:
+        print('ip address sbagliato')
+        ip = input('Inserisci di nuovo IP: ')
+        continue
+    for int in ip_list:
+        if not int.isnumeric() or  int > '255':
+            print('ip address sbagliato')
+            ip = input('Inserisci di nuovo IP: ')
+            break
+#Main programm
+    else:
+        if ip_list == ['255', '255', '255', '255']:
+            print('local broadcast')
+            ip_correct = True
+        elif ip_list == ['0','0','0','0']:
+            print('unassigned')
+            ip_correct = True
+        elif '1' <= oct  <= '223':
+            print('unicast')
+            ip_correct = True
+        elif '224'<= oct<= '239':
+            print('multicast')
+        else:
+            print('unused')
+            ip_correct = True
