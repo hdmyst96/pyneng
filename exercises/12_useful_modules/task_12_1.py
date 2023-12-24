266db5a9b117f6e0f30d314a1f85a8d0eb765f07
hdmyst96 <dmitryhortolomei@gmail.com>
2023-12-24 21:32:09 +0100

    task12_1 done !

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Задание 12.1
@@ -17,3 +18,23 @@ IP-адрес считается доступным, если выполнени
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+
+
+def ping_ip_addresses(ip_list):
+    true_ip = []
+    false_ip = []
+    print('Work in progress...')
+    for ip in ip_list:
+        ping_ips = subprocess.run('ping -c 2 -n {}'.format(ip),shell=True,stdout=subprocess.PIPE)
+        if ping_ips.returncode == 0:
+            true_ip.append(ip)
+        else:
+            false_ip.append(ip)
+    return tuple([true_ip] + [false_ip])
+
+
+if __name__ == "__main__":
+    
+    list_of_ips = ['1.1.1.1', '8.8.8.8', '8.8.4.4', '8.8.7.1']
+    print(ping_ip_addresses(list_of_ips))
